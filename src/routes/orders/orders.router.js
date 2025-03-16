da35145998baf5cd56d4911f6febaca4c66d598f
Abdullah1 <abdullahrifak522@gmail.com>
2025-03-16 22:54:23 +0530

    schema abdhullah

--- src/routes/orders/orders.router.js
+++ src/routes/orders/orders.router.js
@@ -1,10 +1,8 @@
+const express = require("express");
 
-const express = require('express')
+const ordersRouter = express.Router();
+const { getAllOrders } = require("../orders.controller");
 
-const ordersRouter = express.Router()
-const {getAllOrders} = require('../orders.controller')
-
-ordersRouter.get('/orders', getAllOrders)
-
+ordersRouter.get("/orders", getAllOrders);
 
 module.exports = ordersRouter;
