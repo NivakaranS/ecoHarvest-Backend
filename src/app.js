c3fb0d5592d40ffa3695b67b49a94c34da4ccea9
NivakaranS <nivakaran@hotmail.com>
2025-03-22 13:45:28 +0530

    resovling confilicts

--- src/app.js
+++ src/app.js
@@ -3,18 +3,12 @@
 // Importing express
 const express = require('express');
 
-<<<<<<< HEAD
-
-const ordersRouter = require('./routes/orders/orders.router');
-const app = express();
-=======
 const CustomerRouter = require('./routes/customers/customers.router');
 const OrderRouter = require('./routes/orders/orders.router');
->>>>>>> main
 
 const app = express();
 app.use(express.json());
-app.use(ordersRouter)
+
 
 app.use('/customers', CustomerRouter);
 app.use('/orders', OrderRouter);
