fca38fa0ba05dc4d739b3736b0b7c483d016a1df
Abdullah1 <abdullahrifak522@gmail.com>
2025-04-17 17:52:09 +0530

    "discount creation"

--- src/models/discount.model.js
+++ src/models/discount.model.js
@@ -1,3 +1,21 @@
-const discount = require("./discount.mongo");
+const Discount = require("./discount.mongo");
+const Product = require("./products.mongo");
 
-module.exports = discount;
+async function createDiscount(data) {
+  // Optional: Check if product exists before creating
+  console.log(data.productId);
+  const product = await Product.findById(data.productId);
+  if (!product) {
+    throw new Error("Product not found");
+  }
+
+  console.log(data.percentage);
+  return await Discount.create({
+    percentage: data.percentage,
+    category: data.category,
+    status: data.status,
+    //productCategory: data.productCategory,
+    productId: data.productId,
+  });
+}
+module.exports = { createDiscount };
